117a1e7916516fb480ff1fca7ea480d919f76d42
Роман Роман <romanbindyug@yandex.ru>
2022-01-10 19:00:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleLab6_1/Lab6_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLab6_1
+{
+    class Lab6_1
+    {
+        static void Main(string[] args)
+        {
+            string Predlozenie = Console.ReadLine();
+            string[] Massiv = Predlozenie.Split();
+            int L = 0;
+            string slovo = "";
+            foreach (string s in Massiv)
+            {
+                if (s.Length>L)
+                {
+                    L = s.Length;
+                    slovo = s;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Самое большое слово \"{0}\"\nего длина {1} букв", slovo, L);
+            Console.ReadKey();
+        }
+    }
+}
